abea06e6368d060a74838c1a4a9e916c25a2c86f
Grace Lin <51997834+happyg920624@users.noreply.github.com>
2019-06-27 16:14:18 +0800

    Add files via upload

---
+++ 20190617example1.ino
@@ -0,0 +1,24 @@
+/*
+ * Generated using BlocklyDuino:
+ *
+ * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
+ *
+ * Date: Mon, 17 Jun 2019 13:02:20 GMT
+ */
+
+
+
+void setup()
+{
+
+  pinMode(7, OUTPUT);
+}
+
+
+void loop()
+{
+  digitalWrite(7, HIGH);
+  delay(1000);
+  digitalWrite(7, LOW);
+  delay(1000);
+}
